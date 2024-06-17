7e5b37b9c4530c267f26ff1197de36c27fa82639
MaisonEP <erl.maison@hotmail.co.uk>
2024-06-17 11:14:20 +0100

    date calculation, store to local storage,

--- src/app/Components/Form/Form.tsx
+++ src/app/Components/Form/Form.tsx
@@ -2,97 +2,157 @@
 
 import "./Form.css";
 import "react-day-picker/dist/style.css";
-import { FormControl, Input, Stack, Button, Box } from "@chakra-ui/react";
-import { addDays, formatDistance } from "date-fns";
+import {
+	FormControl,
+	Input,
+	Stack,
+	Button,
+	Box,
+	StepNumber,
+} from "@chakra-ui/react";
+import { addDays, formatDistance, formatDistanceStrict } from "date-fns";
 
 import { useEffect, useState } from "react";
 import { DateRange, DayPicker } from "react-day-picker";
 import { distance } from "framer-motion";
 
 export function BnBForm() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [mobile, setMobile] = useState("");
-  const [stayLength, setStayLength] = useState<string | undefined>();
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [mobile, setMobile] = useState("");
+	const [stayLength, setStayLength] = useState<string | undefined>();
 
-  const initialRange: DateRange = {
-    from: new Date(),
-    to: addDays(new Date(), 4),
-  };
+	const initialRange: DateRange = {
+		from: new Date(),
+		to: addDays(new Date(), 4),
+	};
 
-  const [range, setRange] = useState<DateRange | undefined>(initialRange);
+	const [range, setRange] = useState<DateRange | undefined>(initialRange);
 
-  const date = new Date();
+	const date = new Date();
 
-  useEffect(() => {
-    function distance() {
-      if (range?.from !== undefined && range?.to !== undefined) {
-        const rangeDistance = formatDistance(range?.from, range?.to);
-        return rangeDistance;
-      }
-    }
-    const stay = distance();
-    setStayLength(stay);
-  }, [range]);
-  console.log(stayLength);
+	useEffect(() => {
+		function distance() {
+			if (range?.from !== undefined && range?.to !== undefined) {
+				const rangeDistance = formatDistanceStrict(range?.from, range?.to, {
+					unit: "day",
+				});
+				return rangeDistance;
+			}
+		}
+		const stay = distance();
+		setStayLength(stay);
+	}, [range]);
+	console.log(stayLength);
 
-  return (
-    <FormControl isRequired>
-      <div className="Ocean">
-        <div className="island">
-          <Stack className="stackContainer" spacing={2}>
-            <Box
-              bg="#f5b593"
-              borderRadius="32px"
-              // w="100%"
-              p={4}
-              color="white"
-              display={"flex"}
-              justifyContent={"center"}
-            >
-              <DayPicker
-                className="dateSelector"
-                style={{ margin: 0 }}
-                mode="range"
-                selected={range}
-                onSelect={(e) => {
-                  setRange(e);
-                  //   console.log(distance());
-                }}
-                disabled={{ before: new Date() }}
-              />
-            </Box>
-            <Box>
-              {stayLength !== undefined
-                ? `Your stay will be ${stayLength}!`
-                : "Please select stay duration"}
-            </Box>
-            <Box
-              p={4}
-              alignItems={"center"}
-              display={"flex"}
-              flexDirection={"column"}
-            >
-              <Input placeholder="Full Name" size="md" type="text" m={1} />
+	function submitBooking() {
+		if (
+			stayLength !== undefined &&
+			name !== undefined &&
+			email !== undefined &&
+			mobile !== undefined
+		) {
+			const localStoreObj = {
+				stay: stayLength,
+				name: name,
+				email: email,
+				number: mobile,
+			};
+			const serialisedObj = JSON.stringify(localStoreObj);
+			let numberOfEnq = localStorage.getItem("numberOfEnq");
+			if (numberOfEnq === null) {
+				numberOfEnq = "0";
+			}
+			console.log(numberOfEnq);
+			const updatedNumOfEnq = Number(numberOfEnq) + 1;
+			localStorage.setItem("numberOfEnq", updatedNumOfEnq.toString());
+			localStorage.setItem(`enquiry-${updatedNumOfEnq}`, serialisedObj);
+		}
+	}
 
-              <Input placeholder="Email" size="md" type="email" m={1} />
+	return (
+		<FormControl isRequired>
+			<div className="Ocean">
+				<div className="island">
+					<Stack className="stackContainer" spacing={2}>
+						<Box
+							bg="#f5b593"
+							borderRadius="32px"
+							// w="100%"
+							p={4}
+							color="white"
+							display={"flex"}
+							justifyContent={"center"}
+						>
+							<DayPicker
+								className="dateSelector"
+								style={{ margin: 0 }}
+								mode="range"
+								selected={range}
+								onSelect={(e) => {
+									setRange(e);
+									//   console.log(distance());
+								}}
+								disabled={{ before: new Date() }}
+							/>
+						</Box>
+						<Box>
+							{stayLength !== undefined
+								? `Your stay will be ${stayLength}!`
+								: "Please select stay duration"}
+						</Box>
+						<Box
+							p={4}
+							alignItems={"center"}
+							display={"flex"}
+							flexDirection={"column"}
+						>
+							<Input
+								placeholder="Full Name"
+								size="md"
+								type="text"
+								m={1}
+								onChange={(e) => {
+									setName(e.target.value);
+								}}
+							/>
 
-              <Input placeholder="Mobile" size="md" type="number" m={1} />
+							<Input
+								placeholder="Email"
+								size="md"
+								type="email"
+								m={1}
+								onChange={(e) => {
+									setEmail(e.target.value);
+								}}
+							/>
 
-              <Button
-                bg="rgb(135, 78, 50)"
-                color={"white"}
-                variant="solid"
-                size="md"
-                width="20"
-                _hover={{ bg: " rgba(135, 78, 50, 0.813)" }}
-              >
-                Submit
-              </Button>
-            </Box>
-          </Stack>
-        </div>
-      </div>
-    </FormControl>
-  );
+							<Input
+								placeholder="Mobile"
+								size="md"
+								type="number"
+								m={1}
+								onChange={(e) => {
+									setMobile(e.target.value);
+								}}
+							/>
+
+							<Button
+								bg="rgb(135, 78, 50)"
+								color={"white"}
+								variant="solid"
+								size="md"
+								width="20"
+								_hover={{ bg: " rgba(135, 78, 50, 0.813)" }}
+								type="submit"
+								onClick={submitBooking}
+							>
+								Submit
+							</Button>
+						</Box>
+					</Stack>
+				</div>
+			</div>
+		</FormControl>
+	);
 }
