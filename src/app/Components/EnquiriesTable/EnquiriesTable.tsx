4964fac90130e246ba7d84637fe3c66e84ec6942
MaisonEP <erl.maison@hotmail.co.uk>
2024-06-19 00:02:33 +0100

    enquiries table display dynamic data, bnb api with json data file init

--- src/app/Components/EnquiriesTable/EnquiriesTable.tsx
+++ src/app/Components/EnquiriesTable/EnquiriesTable.tsx
@@ -1,70 +1,75 @@
 import { UserResponse } from "@/app/Api/Users/route";
 import {
-	Box,
-	Table,
-	TableCaption,
-	TableContainer,
-	Tbody,
-	Td,
-	Tfoot,
-	Th,
-	Thead,
-	Tr,
+  Box,
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
+  Tfoot,
+  Th,
+  Thead,
+  Tr,
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 
 export function EnquiriesTable() {
-	// stay: stayLength,
-	// name: name,
-	// email: email,
-	// number
+  // stay: stayLength,
+  // name: name,
+  // email: email,
+  // number
 
-	const [data, setData] = useState<UserResponse[] | undefined>();
-	useEffect(() => {
-		async function getEnquiries() {
-			const getEnquiries = await fetch("/Api/Users", {
-				headers: { Accept: "application/json", method: "GET" },
-			});
+  const [data, setData] = useState<UserResponse[] | undefined>();
+  useEffect(() => {
+    async function getEnquiries() {
+      const getEnquiries = await fetch("/Api/Users", {
+        headers: { Accept: "application/json", method: "GET" },
+      });
 
-			const readEnquiries: UserResponse[] = await getEnquiries.json();
-			setData(readEnquiries);
-		}
-		getEnquiries();
-	}, []);
-	// data?.map((e) => {
-	// 	console.log(e.Name);
-	// });
+      const readEnquiries: UserResponse[] = await getEnquiries.json();
+      setData(readEnquiries);
+    }
+    getEnquiries();
+  }, []);
+  // data?.map((e) => {
+  // 	console.log(e.Name);
+  // });
 
-	return (
-		<TableContainer m={"60px"} className="Hi" border={"solid"}>
-			<Table variant="simple">
-				<TableCaption>Enquiries made by clients!</TableCaption>
-				<Thead>
-					<Tr>
-						<Th>Client Name</Th>
-						<Th>Email</Th>
-						<Th> Contact Number</Th>
-						<Th> Stay Duration</Th>
-						<Th> Revenue</Th>
-					</Tr>
-				</Thead>
-				<Tbody>
-					<Tr>
-						{data?.map((e) => {
-							return <tr>{e.Name}</tr>;
-						})}
-						{/* {data?.map((data) => {
-							console.log("this is data  " + data);
-							return <p></p>;
-						})} */}
-						<Td>Erl</Td>
-						<Td>Erl@email.com</Td>
-						<Td>0781385500</Td>
-						<Td>6 days</Td>
-						<Td>£200</Td>
-					</Tr>
-				</Tbody>
-			</Table>
-		</TableContainer>
-	);
+  return (
+    <TableContainer m={"60px"} className="Hi" border={"solid"}>
+      <Table variant="simple">
+        <TableCaption>Enquiries made by clients!</TableCaption>
+        <Thead>
+          <Tr>
+            <Th>
+              Client <br /> Name
+            </Th>
+            <Th>Email</Th>
+            <Th>
+              Contact <br />
+              Number
+            </Th>
+            <Th>
+              Stay <br />
+              Duration (Days)
+            </Th>
+            <Th> Revenue</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {data?.map((e) => {
+            return (
+              <tr>
+                <Td>{e.Name}</Td>
+                <Td>{e.Email}</Td>
+                <Td>{e.Mobile}</Td>
+                <Td>{e.StayLength}</Td>
+                <Td>{"£" + e.Revenue}</Td>
+              </tr>
+            );
+          })}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
 }
