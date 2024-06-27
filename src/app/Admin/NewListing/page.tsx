26bdbeb4152f71d70562665e962979ea61c3edad
MaisonEP <erl.maison@hotmail.co.uk>
2024-06-27 21:11:45 +0100

    listing page, dynamic query params, bnb data storage, carousel finish, routes

---
+++ src/app/Admin/NewListing/page.tsx
@@ -0,0 +1,10 @@
+import ListingForm from "@/app/Components/ListingForm/NewListing";
+import { Box } from "@chakra-ui/react";
+
+export default function NewListing() {
+	return (
+		<Box height={"100vh"} width={"100vw"}>
+			<ListingForm></ListingForm>
+		</Box>
+	);
+}
